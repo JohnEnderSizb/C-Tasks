ef75e3c4c149e2f1b62f453fcd401be6c8197681
Johnson A.Siziba <siziba.uz@gmail.com>
2020-02-04 12:11:05 +0200

    is it done yet?

--- Program.cs
+++ Program.cs
@@ -1,49 +1,81 @@
-﻿using System;   
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 class Program
 {
-    static Random rand = new Random();
-
-    static void Main()
+  public static void Main()
+  { 
+    TcpListener server=null;   
+    try
     {
-        // Wait on a single task with no timeout specified.
-        Task taskA = Task.Run( () =>  {
-        	
-        	//DownloadFileFromFTP.download();
-        	UpdateBalances.update();
-        	}
-    	);
+      // Set the TcpListener on port 13000.
+      Int32 port = 13000;
+      IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+      
+      // TcpListener server = new TcpListener(port);
+      server = new TcpListener(localAddr, port);
 
-        Console.WriteLine("taskA Status: {0}", taskA.Status);
-        try {
-          taskA.Wait();
-          Console.WriteLine("taskA Status: {0}", taskA.Status);
-       } 
-       catch (AggregateException) {
-          Console.WriteLine("Exception in taskA.");
-       } 
-    }    
+      // Start listening for client requests.
+      server.Start();
+         
+      // Buffer for reading data
+      Byte[] bytes = new Byte[256];
+      String data = null;
 
+      // Enter the listening loop.
+      while(true) 
+      {
+        Console.Write("Waiting for a connection... ");
+        
+        // Perform a blocking call to accept requests.
+        // You could also user server.AcceptSocket() here.
+        TcpClient client = server.AcceptTcpClient();            
+        Console.WriteLine("Connected!");
 
+        data = null;
 
+        // Get a stream object for reading and writing
+        NetworkStream stream = client.GetStream();
 
+        int i;
 
+        // Loop to receive all the data sent by the client.
+        while((i = stream.Read(bytes, 0, bytes.Length))!=0) 
+        {   
+          BalancesMain.startUpdates();
 
+          // Translate data bytes to a ASCII string.
+          data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+          Console.WriteLine("Received: {0}", data);
+       
+          // Process the data sent by the client.
+          data = data.ToUpper();
 
+          byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
+          // Send back a response.
+          stream.Write(msg, 0, msg.Length);
+          Console.WriteLine("Response: {0}", data);            
+        }
+         
+        // Shutdown and end connection
+        client.Close();
+      }
+    }
+    catch(SocketException e)
+    {
+      Console.WriteLine("SocketException: {0}", e);
+    }
+    finally
+    {
+       // Stop listening for new clients.
+       server.Stop();
+    }
 
-
-
-
-
-
-
-
+    Console.WriteLine("\nHit enter to continue...");
+    Console.Read();
+  }   
 }
-
-/*
-
-
-*/
